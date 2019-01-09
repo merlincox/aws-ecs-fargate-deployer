c46ef0614e6a6ec2cd00bacfce5695d26264c24b
Merlin Cox <merlin.cox@denma.digital>
2019-01-09 09:43:28 +0000

    server message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const dateobj = new Date();
 const datestamp = dateobj.toISOString();;
 const port = process.env.ECS_PORT || 3000;
-const message = process.env.RELEASE ? `${process.env.PLATFORM} platform. Release: ${process.env.RELEASE} built from ${process.env.BRANCH} branch at commit ${process.env.COMMIT} running at ${datestamp} on port ${port}` :  `Local platform. Running locally at ${datestamp} on port ${port}`;
+const message = process.env.RELEASE ? `This is the ${process.env.PLATFORM} platform. Release: ${process.env.RELEASE} built from ${process.env.BRANCH} branch at commit ${process.env.COMMIT} running at ${datestamp} on port ${port}` :  `Local platform. Running locally at ${datestamp} on port ${port}`;
 
 app.get('/', (req, res) => {
     res.send(message)
